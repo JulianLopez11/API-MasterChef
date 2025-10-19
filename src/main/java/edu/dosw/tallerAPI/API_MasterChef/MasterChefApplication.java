e13bcd4b0c88bebadcb2363a7cd84d39037a6c35
Julian <julianl116910@gmail.com>
2025-10-18 20:52:31 -0500

    pom funcionando

---
+++ src/main/java/edu/dosw/tallerAPI/API_MasterChef/MasterChefApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.tallerAPI.API_MasterChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/tallerAPI/API_MasterChef/ApiMasterChefApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.tallerAPI.API_MasterChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMasterChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
